6045a138571792f0f2099282a22e95861d997e92
Marcela <mrssolaris.dev@gmail.com>
2020-01-17 15:44:57 -0300

    Simulador de balanceamento de simbolos

---
+++ facul/est_dados/src/apsest/Inicio.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apsest;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author Roverson
+ */
+public class Inicio {
+
+    public static void main(String args[]) throws FileNotFoundException, Exception{
+       TelaInicial tela = new TelaInicial();
+       tela.setVisible(true);      
+    }
+}
